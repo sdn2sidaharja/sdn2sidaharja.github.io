f7c4b2756df4d6e463d7082b3b6aea3a5cd55768
septian98 <63330699+septian98@users.noreply.github.com>
2022-06-13 01:27:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,151 @@
+// function validateDaftar() {
+//     let a = document.forms["ppdbonline"]["inputNama"].value;
+//     if (a.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "Nama Lengkap tidak boleh kosong!",
+//       });
+//       return false;
+//     }
+
+//     let b = document.forms["ppdbonline"]["inputNomorNik"].value;
+//     if (b.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let c = document.forms["ppdbonline"]["inputTempatLahir"].value;
+//     if (c.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let d = document.forms["ppdbonline"]["inputTanggalLahir"].value;
+//     if (d.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let e = document.forms["ppdbonline"]["inputNomorKK"].value;
+//     if (e.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let f = document.forms["ppdbonline"]["inputNamaAyah"].value;
+//     if (f.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let g = document.forms["ppdbonline"]["inputNomorNikAyah"].value;
+//     if (g.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let h = document.forms["ppdbonline"]["inputNamaIbu"].value;
+//     if (h.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let i = document.forms["ppdbonline"]["inputNomorNikIbu"].value;
+//     if (i.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let j = document.forms["ppdbonline"]["inputNomorTelepon"].value;
+//     if (j.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let k = document.forms["ppdbonline"]["inputDesa"].value;
+//     if (k.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let l = document.forms["ppdbonline"]["inputRt"].value;
+//     if (l.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let m = document.forms["ppdbonline"]["inputRw"].value;
+//     if (m.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let n = document.forms["ppdbonline"]["inputKecamatan"].value;
+//     if (n.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+
+//     let o = document.forms["ppdbonline"]["inputKabupaten"].value;
+//     if (o.trim() == "") {
+//       Swal.fire({
+//         icon: "error",
+//         title: "ERROR",
+//         text: "NIK wajib diisi!",
+//       });
+//       return false;
+//     }
+// }
